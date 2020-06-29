28482f1c004a7ca99bf49558a9938205cd7bc538
dave-v-qr <david.vaughan@quickrelease.co.uk>
2020-06-29 10:37:37 +0100

    ADD: Setup and requirements.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='cypress-db-helper',
+    version='0.0.1',
+    author='David Vaughan',
+    author_email='david.vaughan@quickrelease.co.uk',
+    description='Cypress DB Helper command line script',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/QuickRelease/cypress-db-helper.git',
+    packages=find_packages(),
+)
